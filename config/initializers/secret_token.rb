e635e15ac327b65d8078ed233cee6b642f329cb7
Milind S. Pandit <mspandit@imac-2.ftrdhcpuser.net>
2011-10-22 09:03:53 -0700

    Initial commit with rspec, guard, and simplecov

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paydici::Application.config.secret_token = '73d6958f85844c4ec338efd9780bd923e044b5ae21cd7b9a8669ca714eb9597adf11f5eafce0b498e60087211ded6582ebeadf3430abf8ea7e9eaf59da4b5b6b'
